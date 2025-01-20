e3b162725d7a699937b646049b8a625c354d7b28
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2025-01-20 21:00:29 +0100

    20

--- plugins/directive.ts
+++ plugins/directive.ts
@@ -1,20 +1,28 @@
 function toNaira(val: number) {
-    return new Intl.NumberFormat('en-NG', {
-        style: 'currency',
-        currency: 'NGN',
-        maximumFractionDigits: 2
-    }).format(val)
+  return new Intl.NumberFormat("en-NG", {
+    style: "currency",
+    currency: "NGN",
+    maximumFractionDigits: 2,
+  }).format(val);
 }
 export default defineNuxtPlugin((nuxtApp) => {
-    if (import.meta.server) {
-        return 
-    }
+  if (import.meta.server) {
     nuxtApp.vueApp.directive("naira", {
-        mounted (el, binding){
-            el.innerText = toNaira(binding.value)
-        },
-        updated (el, binding){
-            el.innerText = toNaira(binding.value)
-        }
-    })
-})
+      mounted(el, binding) {
+        el.innerText = "NGN" + el.innerText;
+      },
+      updated(el, binding) {
+        el.innerText = "NGN" + el.innerText;
+      },
+    });
+  } else {
+    nuxtApp.vueApp.directive("naira", {
+      mounted(el, binding) {
+        el.innerText = toNaira(binding.value);
+      },
+      updated(el, binding) {
+        el.innerText = toNaira(binding.value);
+      },
+    });
+  }
+});
